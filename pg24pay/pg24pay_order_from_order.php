9cafe01197b9066d1c46ae5ad57d649f7daf8270
24pay <durcak@24-pay.eu>
2024-05-23 10:36:07 +0200

    Update pg24pay_order_from_order.php

--- pg24pay/pg24pay_order_from_order.php
+++ pg24pay/pg24pay_order_from_order.php
@@ -73,9 +73,12 @@ class Pg24payOrderFromOrder {
         $this->country = $this->convertCountryCodeToIsoA3($country->iso_code);
                 
         $this->amount = number_format($objOrder->total_paid_real, 2, '.', '');
-        
+        /*
         $this->rurl = Tools::getHttpHost(true) . __PS_BASE_URI__ . 'module/pg24pay/rurl';
         $this->nurl = Tools::getHttpHost(true) . __PS_BASE_URI__ . 'module/pg24pay/ordernurl';
+        */
+        $this->rurl = Context::getContext()->link->getModuleLink('pg24pay','rurl',[]);
+		$this->nurl = Context::getContext()->link->getModuleLink('pg24pay','nurl',[]);
     }
     
     private function convertCountryCodeToIsoA3($isoa2code) {
